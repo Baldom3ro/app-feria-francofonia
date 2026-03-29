028e8ab14df40273872f43f71170a10d7a266d6c
Baldomero <23610049@utgz.edu.mx>
2026-03-28 20:54:04 -0600

    correción de logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,6 @@
 
 namespace App\Providers;
 
-use App\Listeners\LogAuthenticationEvents;
-use Illuminate\Auth\Events\Failed;
-use Illuminate\Auth\Events\Lockout;
-use Illuminate\Auth\Events\Login;
-use Illuminate\Auth\Events\Logout;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,11 +23,6 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
-
-        // Register authentication event listeners for logging
-        Event::listen(Login::class, [LogAuthenticationEvents::class, 'handleLogin']);
-        Event::listen(Failed::class, [LogAuthenticationEvents::class, 'handleFailed']);
-        Event::listen(Logout::class, [LogAuthenticationEvents::class, 'handleLogout']);
-        Event::listen(Lockout::class, [LogAuthenticationEvents::class, 'handleLockout']);
     }
 }
+
